e505a78cef2e2aaa93836dfaa32e5ba6dfb4ce6f
gitaroktato <gitaroktato@gmail.com>
2017-05-12 21:01:18 +0200

    Adding gradle wrapper.

---
+++ src/main/java/com/epam/example/CompletablefutureApplication.java
@@ -0,0 +1,12 @@
+package com.epam.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompletablefutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompletablefutureApplication.class, args);
+	}
+}

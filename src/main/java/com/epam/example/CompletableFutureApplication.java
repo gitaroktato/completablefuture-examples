8f23d1e8543b2bf379c9d0604d2585411a94abe9
gitaroktato <gitaroktato@gmail.com>
2017-05-12 22:18:18 +0200

    Tomcat config

--- src/main/java/com/epam/example/CompletableFutureApplication.java
+++ src/main/java/com/epam/example/CompletableFutureApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CompletablefutureApplication {
+public class CompletableFutureApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CompletablefutureApplication.class, args);
+		SpringApplication.run(CompletableFutureApplication.class, args);
 	}
 }

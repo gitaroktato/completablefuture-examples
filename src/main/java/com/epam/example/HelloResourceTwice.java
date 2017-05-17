a055b909c86fc18426476b7fd961e6dbc17f3aba
gitaroktato <gitaroktato@gmail.com>
2017-05-17 23:21:45 +0200

    Calling bad method twice, no bulkheads.

---
+++ src/main/java/com/epam/example/HelloResourceTwice.java
@@ -0,0 +1,38 @@
+package com.epam.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+@Component
+@Path("/twice")
+public class HelloResourceTwice {
+
+    @Autowired
+    private SlowService slowService;
+
+    @GET
+    @Path("/hello_async")
+    public void helloAsync(@Suspended AsyncResponse response)
+            throws InterruptedException {
+        final CompletableFuture<String> callSlowService1, callSlowService2;
+        callSlowService1 = CompletableFuture.supplyAsync(slowService::getMessage);
+        callSlowService2 = CompletableFuture.supplyAsync(slowService::getMessage);
+        callSlowService1.thenAcceptBothAsync(callSlowService2,
+                (t,u) -> response.resume(t + u));
+    }
+
+    @GET
+    @Path("/hello_sync")
+    public String helloSync() throws InterruptedException {
+         return slowService.getMessage() + slowService.getMessage();
+    }
+
+}

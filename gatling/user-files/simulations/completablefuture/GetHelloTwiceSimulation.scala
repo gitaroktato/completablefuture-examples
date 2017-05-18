ecfbb86250833d2925146a6b0f1787bd413b3563
gitaroktato <gitaroktato@gmail.com>
2017-05-18 11:32:53 +0200

    Calling bad method twice, no bulkheads.

---
+++ gatling/user-files/simulations/completablefuture/GetHelloTwiceSimulation.scala
@@ -0,0 +1,39 @@
+package completablefuture
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class GetHelloTwiceSimulation extends Simulation {
+
+	val httpProtocol = http
+		.baseURL("http://localhost:8080/twice")
+		.inferHtmlResources()
+		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("en-US,en;q=0.5")
+		.userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:50.0) Gecko/20100101 Firefox/50.0")
+
+	val headers = Map("Upgrade-Insecure-Requests" -> "1")
+
+	val times = 25
+	val helloSync = repeat(times, "n") {
+		exec(http("hello_sync")
+			.get("/hello_sync")
+			.headers(headers))
+	}
+    val helloAsync = repeat(times, "n") {
+        exec(http("hello_async")
+                .get("/hello_async")
+                .headers(headers))
+    }
+	val sync = scenario("Get Hello").exec(helloSync);
+    val async = scenario("Get Hello Async").exec(helloAsync);
+
+	setUp(
+        sync.inject(rampUsers(75) over (3 seconds)),
+        async.inject(rampUsers(75) over (3 seconds))
+	).protocols(httpProtocol)
+}
